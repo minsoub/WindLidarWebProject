9ed90dee92c5112ff41a00532d150792ac2ba530
minsoub <mjoung@hist.co.kr>
2017-03-23 17:24:46 +0900

    eeeee

--- WindLidarWebProject/src/main/java/com/hist/windlidar/service/WindLidarService.java
+++
@@ -1,8 +0,0 @@
-package com.hist.windlidar.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface WindLidarService {
-	List<Map<String, Object>> selectWindLidarList(Map<String, Object> commandMap)  throws Exception;
-}
